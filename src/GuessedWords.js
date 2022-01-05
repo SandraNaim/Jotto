9b4dcee061b081ac2a01b3dd0e0d4cacac894515
Sandra Naim <sandra.naim@toothpickapp.com>
2022-01-05 13:43:15 +0200

    create guessed words test then component in case there are words guessed

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -9,6 +9,27 @@ const GuessedWords = (props) => {
                 Try to guess the secret word!
             </span>
         )
+    } else {
+        let guessedWordsRow = props.guessedWords.map((word, index) => (
+            <tr data-test="guessed-word" key={index}>
+                <td>{word.guessedWord}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ))
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed words</h3>
+                <table>
+                    <thead>
+                        <tr><th>Guess</th><th>Matching Letters</th></tr>
+                    </thead>
+                    <tbody>
+                        {guessedWordsRow}
+                    </tbody>
+                </table>
+
+            </div>
+        ) 
     }
 
     return (
