bb228ed4eda43997ceb065f0ac5124186d0fd803
Sandra Naim <sandra.naim@toothpickapp.com>
2022-01-07 16:22:06 +0200

    create a __mock__ file to mock the getSecretWord function

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import App from './App';
+import { getSecretWord as mockGetSecretWord } from './actions';
+import { findByTestAttr } from '../test/testUtils';
 
-import { checkProps, findByTestAttr } from '../test/testUtils';
+// activate global mock to make sure getSecrteWord dosen't make netword call
+jest.mock('./actions')
 
 const setup = () => {
-    return shallow(<App />)
+  // use mount instead of shallow because useEffect is not called on shallow
+    return mount(<App />)
 } 
 
 test('renders without error', () => {
@@ -13,3 +17,26 @@ test('renders without error', () => {
   const appComponent = findByTestAttr(wrapper, 'component-app')
   expect(appComponent).toHaveLength(1);
 });
+
+describe('get secret word', () => {
+  beforeEach(() => {
+    // clear the mock calls from previous state
+    mockGetSecretWord.mockClear();
+  })
+
+  test('getSecretWord on app mount', () => {
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+
+  test('getSecretWord does not run on app update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+
+    // using setProps because wrapper.update() does not trigger useEffect
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
+
+})
