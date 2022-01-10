48169eb5795677020b990f41c5272fe8c022f559
Sandra Naim <sandra.naim@toothpickapp.com>
2022-01-10 15:57:48 +0200

    create the reducers for the guess word, secret word and success functions

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -2,9 +2,9 @@ import { actionTypes } from "../actions"
 
 export default (state=false, action) => {
     switch(action.type) {
-        case (actionTypes.CORRECT_GUESS):
-            return true
-        default:
-            return state
+      case (actionTypes.CORRECT_GUESS):
+        return true;
+      default:
+        return state;
     }
 }
