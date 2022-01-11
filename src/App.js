d17a9fc83e3396fec8af32650aa1ca8cf09b88a4
Sandra Naim <sandra.naim@toothpickapp.com>
2022-01-11 22:02:48 +0200

    call the total guesses, secret word reveal and the reset button components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getSecretWord } from './actions';
+import { getSecretWord, resetGame } from './actions';
 import './App.css';
 import Congrats from './Congrats';
 import GuessedWords from './GuessedWords';
 import Input from './Input';
+import ResetButton from './ResetButton';
+import SecretWordReveal from './SecretWordReveal';
+import TotalGuesses from './TotalGuesses';
 
 function App() {
   const success = useSelector(state => state.success);
   const guessedWords = useSelector(state => state.guessedWords);
   const secretWord = useSelector(state => state.secretWord);
+  const gaveUp = useSelector((state) => state.gaveUp);
 
   const dispatch = useDispatch();
 
@@ -21,8 +25,14 @@ function App() {
     <div data-test="component-app" className="container">
       <h1>Jotto</h1>
       <Congrats success={success} />
+      <SecretWordReveal display={gaveUp} secretWord={secretWord} />
+      <ResetButton
+        display={success || gaveUp}
+        resetAction={() => dispatch(resetGame())}
+      />
       <Input success={success} secretWord={secretWord} />
       <GuessedWords guessedWords={guessedWords} />
+      <TotalGuesses guessedWords={guessedWords} />
     </div>
   );
 }
