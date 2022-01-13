4785cccde7bc1722dbda99526bd72be8629e4040
Sandra Naim <sandra.naim@toothpickapp.com>
2022-01-13 14:13:19 +0200

    install redux-mock-store to test the actions and to make sure that it is dispatcing the right action

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,6 +1,10 @@
 import moxios from "moxios";
-import { getSecretWord } from ".";
+import { actionTypes, enterOwnSecretWord, getSecretWord, giveUp, resetGame, setUserEntering } from ".";
 import { storeFactory } from "../../test/testUtils";
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from  'redux-thunk';
+
+const mockStore = configureMockStore([thunkMiddleware])
 
 describe('getSecretWord', () => {
 
@@ -27,5 +31,37 @@ describe('getSecretWord', () => {
                 const secretWord = store.getState().secretWord;
                 expect(secretWord).toBe('party');
             })
+    })
+})
+
+describe('giveUp', () => {
+    test('should create action to give up', () => {
+        expect(giveUp()).toEqual({ type: actionTypes.GIVE_UP })
+    })
+})
+
+describe('setUserEntering', () => {
+    test('should create action to give up', () => {
+        expect(setUserEntering()).toEqual({ type: actionTypes.USER_ENTERING })
+    })
+})
+
+describe('resetGame', () => {
+    test('should create a rest game action', () => {
+        const store = mockStore();
+        store.dispatch(resetGame())
+        const action = store.getActions();
+        expect(action[0]).toEqual({type: "RESET_GAME"});
+    })
+})
+
+describe('enterOwnSecretWord', () => {
+    test('should create set secret word and user own secret word action', () => {
+        const store = mockStore();
+        let userSecretWord = 'train';
+        store.dispatch(enterOwnSecretWord(userSecretWord))
+        const action = store.getActions();
+        expect(action[0]).toEqual({ type: actionTypes.SET_SECRET_WORD, payload:'train' });
+        expect(action[1]).toEqual({ type: actionTypes.ENTER_OWN_SECRET_WORD });
     })
 })
